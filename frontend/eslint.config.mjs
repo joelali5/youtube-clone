45a7631a47e2f9e6ad0a8184230d41417c435b1c
Joel Aliyu <joelaliyu1@gmail.com>
2025-05-07 18:37:18 +0100

    Fix: ci/cd pipeline

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,53 +1,14 @@
-// eslint.config.mjs
-import next from '@next/eslint-plugin-next';
-import js from '@eslint/js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default [
-  {
-    // Applies Next.js-specific rules
-    plugins: {
-      '@next/next': next
-    },
-    rules: {
-      ...next.configs.recommended.rules,
-      // Add your custom rules here
-    }
-  },
-  {
-    // Applies standard ESLint rules
-    ...js.configs.recommended,
-    rules: {
-      // Additional global rules
-    }
-  }
-];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
+const eslintConfig = [...compat.extends('next/core-web-vitals')];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-// import { FlatCompat } from '@eslint/eslintrc';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [...compat.extends('next/core-web-vitals')];
-
-// export default eslintConfig;
+export default eslintConfig;
